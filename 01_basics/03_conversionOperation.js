82a9bd3cb0254ec3d47ef7232cf66d4a5bc2b0e0
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-04-30 11:20:06 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score="33"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+
+//"33"=>33
+//"22abc"=>NaN
+//true=>1,false=>0
+//null=>0
+//undefined=>NaN
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//""=>false
+//"ankit"=>true
