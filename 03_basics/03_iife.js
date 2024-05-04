bebcac2dab10b6dde432cf7f214da3bdbfc908c7
gupta-ankit123 <139711446+gupta-ankit123@users.noreply.github.com>
2024-05-04 15:34:53 +0000

    iife

---
+++ 03_basics/03_iife.js
@@ -0,0 +1,13 @@
+//immediately involed function expression
+
+(function chai (){
+    //named iife
+    console.log("DB Connected");
+})();//yaha pe semicolon lagana jaruri hai tab hi pata chalega code ko ki ab function ko rok dena hai
+//iife ke use karte hi function immediately invoke ho jata hai
+//iska use isliye karte hai kyuki global scope ke variables se pollution hoti hai to usko hatane ke liye iife ka use kar rahe hai
+// chai()
+
+((name)=>{
+    console.log(`DB Connected TWO ${name}`);
+})('Ankit');
